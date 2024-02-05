1180017f78cf406330ff8fe65d2369a1501344e2
GambitChessClub <contact@gambitclubnft.com>
2024-02-05 16:51:55 +0100

    updated constants for production and increased limit for number of tokens allowed for each wallet

--- assembly/Collections.ts
+++ assembly/Collections.ts
@@ -219,7 +219,7 @@ export class Collections {
     balance.value = SafeMath.add(balance.value, args.number_tokens_to_mint);
 
     // check limit address
-    System.require(balance.value <= 10, "exceeds the limit of tokens per address");
+    System.require(balance.value <= 151, "exceeds the limit of tokens per address");
 
     // increment supply
     supply.value = SafeMath.add(supply.value, args.number_tokens_to_mint);
--- assembly/Constants.ts
+++ assembly/Constants.ts
@@ -6,10 +6,10 @@ export namespace Constants {
   export const MINT_PRICE: u64 = 9652500000;
   export const MINT_FEE: bool = true;
   export const MAX_SUPPLY: u64 = 150;
-  export const URI: string = "https://bafybeibknkktyd2yvcla3m6ldgyivupsd6gtrfo3x2l42ufh2tht47fcxm.ipfs.nftstorage.link/";
+  export const URI: string = "https://bafybeihcpj6v6ykgif2z6ukjss5gkasmzt7haztd3zdpy5iy7pirgcv3ee.ipfs.nftstorage.link/";
   export const OWNER: Uint8Array = Base58.decode("1E6EKDGSeXsZKhX4DPTNxY8FWvCQnTBbU6");
 
   // token mint
-  export const TOKEN_PAY: Uint8Array = Base58.decode("1FaSvLjQJsCJKq5ybmGsMMQs8RQYyVv8ju");
+  export const TOKEN_PAY: Uint8Array = Base58.decode("15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL");
   export const ADDRESS_PAY: Uint8Array = Base58.decode("1E6EKDGSeXsZKhX4DPTNxY8FWvCQnTBbU6");
 }
